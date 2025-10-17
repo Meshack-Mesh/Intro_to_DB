f5d1c035e482c2bec07eefc3f67e3b713445c594
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-10-17 18:36:20 +0300

    Introduction to DB

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- Insert a single record into the customers table
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
-VALUES (1, 'Michael Bodo', 'meshackmwima@gmail.com', 'Moi Avenue');
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
